4b68e292a19247b5b66de2e4d94a036d70756170
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-24 16:51:57 -0300

    feat: :sparkles: paginação e barra de buscas finalizadas

--- desafio-pluga/src/pages/Home.jsx
+++ desafio-pluga/src/pages/Home.jsx
@@ -5,21 +5,32 @@ import Search from '../components/Search';
 import api from '../services/api';
 
 function Home() {
-  const [cards, setCards] = useState([]);
+  // const [cards, setCards] = useState([]);
   const [search, setSearch] = useState('');
+  const [tools, setTools] = React.useState([]);
+  const [filter, setFilter] = React.useState([]);
   const [upPage, setUpPage] = useState(0);
   const [endPage, setEndPage] = useState(11);
 
   useEffect(() => {
-   api.get()
-   .then((response) => setCards(response.data))
-   .catch((err) => {
-    console.error("error" + err);
-   });
+    async function loadTools() {
+      const response = await api();
+      setTools(response.data);
+      setFilter(response.data);
+    }
+    loadTools();
   }, []);
+
+  const searchTool = (word) => {
+    const newTool = tools
+    .filter((value) => (value.name.toLowerCase()
+    .includes(word.toLowerCase())));
+    setFilter(newTool);
+  }
 
   const handleChange = ({target}) => {
     setSearch(target.value);
+    searchTool(target.value);
   }
 
   const nextPage = () => {
@@ -31,15 +42,17 @@ function Home() {
     setUpPage(upPage - 12);
     setEndPage(endPage - 12);
   }
+  
   return (
     <div>
       <div>
         <Search
         state={search}
-        onChange={handleChange}
+        callback={handleChange}
         />
         {
-          cards.map(({ app_id, name, color, icon, link }) => (
+          tools.length ? filter
+          .filter((_element, index) => index >= upPage && index <= endPage).map(({ app_id, name, color, icon, link }) => (
             <Cards
               key={app_id}
               name={name}
@@ -47,16 +60,15 @@ function Home() {
               icon={icon}
               link={link}
             />
-          ))
-        }
+          )) : <span>Carregando...</span> }
       </div>
 
       <div>
-        <button type="button" onClick={backPage}>
+        <button type="button" onClick={backPage} disabled={upPage <= 0}>
           <span>Voltar</span>
         </button>
 
-        <button type="button" onClick={nextPage}>
+        <button type="button" onClick={nextPage} disabled={endPage > tools.length - 1}>
           <span>Proxima</span>
         </button>
       </div>
