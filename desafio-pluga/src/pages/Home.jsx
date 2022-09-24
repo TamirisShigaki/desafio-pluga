3a67ab1b67ea813f2a034349c3617f680e43839e
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-24 16:02:07 -0300

    feat: :construction: adicionando botão de paginação
    falta estruturar a quantidade de card renderizado na tela

--- desafio-pluga/src/pages/Home.jsx
+++ desafio-pluga/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Cards from '../components/Cards';
+import Search from '../components/Search';
 import api from '../services/api';
 
 function Home() {
   const [cards, setCards] = useState([]);
-  // const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [upPage, setUpPage] = useState(0);
+  const [endPage, setEndPage] = useState(11);
 
   useEffect(() => {
    api.get()
@@ -15,19 +18,48 @@ function Home() {
    });
   }, []);
 
+  const handleChange = ({target}) => {
+    setSearch(target.value);
+  }
+
+  const nextPage = () => {
+    setUpPage(upPage + 12);
+    setEndPage(endPage + 12);
+  }
+
+  const backPage = () => {
+    setUpPage(upPage - 12);
+    setEndPage(endPage - 12);
+  }
   return (
     <div>
-      {
-        cards.map(({ app_id, name, color, icon, link }) => (
-          <Cards
-            key={app_id}
-            name={name}
-            color={color}
-            icon={icon}
-            link={link}
-          />
-        ))
-      }
+      <div>
+        <Search
+        state={search}
+        onChange={handleChange}
+        />
+        {
+          cards.map(({ app_id, name, color, icon, link }) => (
+            <Cards
+              key={app_id}
+              name={name}
+              color={color}
+              icon={icon}
+              link={link}
+            />
+          ))
+        }
+      </div>
+
+      <div>
+        <button type="button" onClick={backPage}>
+          <span>Voltar</span>
+        </button>
+
+        <button type="button" onClick={nextPage}>
+          <span>Proxima</span>
+        </button>
+      </div>
     </div>
   );
 }
