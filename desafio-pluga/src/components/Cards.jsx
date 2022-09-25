158d9d8e4cc5c41960661c8e8135ad54d39cd2e1
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-24 21:25:50 -0300

    feat: :lipstick: adicionando css ao Card

--- desafio-pluga/src/components/Cards.jsx
+++ desafio-pluga/src/components/Cards.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Cards.css';
 
-function Cards({name, color, icon, link }) {
+function Cards({name, color, icon, link, open }) {
   return (
     <div className="card-container">
+      <button className="card-button" type="button" onClick={open}>
       <div className="card">
+
       <div className="card-img" style={{ backgroundColor: `${color}` }}>
         <img src={icon} alt={name}/>
       </div>
+
       <div className="card-link">
         <a className="card-name"href={link} target='_blank' rel="noreferrer">{ name }</a>
       </div>
+
       </div>
+      </button>
     </div>
   );
 }
@@ -22,6 +26,7 @@ Cards.propTypes = {
     color: PropTypes.string,
     icon: PropTypes.string,
     link: PropTypes.string,
+    open: PropTypes.func,
 }.isRequired;
 
 export default Cards;
