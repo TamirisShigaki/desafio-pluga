53bc3b1f54942ac44def10c395bf68cfccd32771
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-24 21:26:59 -0300

    feat: :lipstick: adicionando css ao modal

--- desafio-pluga/src/components/ModalCard.jsx
+++ desafio-pluga/src/components/ModalCard.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
 
 function ModalCard() {
     const lStorage = JSON.parse(localStorage.getItem('tools'));
   return (
-    <div>
+    <div className="modal-container">
         {
             lStorage.map(({ name, icon, color, link }, index) => {
                 if (index === 0) {
                     return(
                         <div>
-                            <div>
-                                <img src={icon} alt={name} />
-                            </div>
-                            <div>
-                                <span>{name}</span>
-                                <button type="button"><a href={link} target="_blank" rel="noreferrer">Acessar</a></button>
+                            <div className="modal">
+                                <div className="modal-img" style={{
+                    backgroundColor: `${color}`,
+                  }}>
+                                    <img src={icon} alt={name} />
+                                </div>
+                                <div>
+                                    <span>{name}</span>
+                                    <button className="modal-link" type="button"><a className="modal-acess" href={link} target="_blank" rel="noreferrer">Acessar</a></button>
+                                </div>
                             </div>
                             <h3>ÚLTIMAS FERRAMENTAS VISUALIZADAS</h3>
                         </div>
@@ -23,7 +26,7 @@ function ModalCard() {
                 return(
                     <div>
                         <div>
-                            <div style={{ backgroundColor: `${color}` }}>
+                            <div className="modal-img" style={{ backgroundColor: `${color}` }}>
                                 <img src={icon} alt={name}/>
                             </div>
                             
